6845ca980be4e7ead32f5af7850d445c531fd318
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-10-11 15:59:51 -0400

    Completed Debugging

---
+++ Week8/PathException.java
@@ -0,0 +1,9 @@
+package Week8;
+@SuppressWarnings("serial")
+public class PathException extends Exception {
+	
+	  public PathException(String errorMessage) { super(errorMessage); }
+	  
+	  public PathException() { super(); }
+	  
+}

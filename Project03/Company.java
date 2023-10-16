629299b6b3063fcc4edb3a35a16e14606d9184af
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-10-16 18:39:04 -0400

    Finished Company.java

--- Project03/Company.java
+++ Project03/Company.java
@@ -1,5 +1,24 @@
 package Project03;
 
+/**Company.java
+ * 
+ * Records data associated with a single company.
+ * 
+ */
 public class Company {
-    
+    private String name;    // name of company
+    private int[] prices;   // Stock prices associated with the company
+
+    public Company(String name, int[] prices) {
+        this.name = name;
+        this.prices = prices;
+    }
+
+    // Name getter and setter
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    // Prices getter and setter
+    public int[] getPrices() { return prices; }
+    public void setPrices(int[] prices) { this.prices = prices; }
 }

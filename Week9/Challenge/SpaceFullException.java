778761c56523ed9c6c64dd3984f196d8f3a0be16
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-10-17 22:03:54 -0400

    Exception child classes' Javadoc

--- Week9/Challenge/SpaceFullException.java
+++ Week9/Challenge/SpaceFullException.java
@@ -1,5 +1,11 @@
 package Week9.Challenge;
-
+/**SpaceFullException.java
+ * 
+ * Thrown when the ride is full.
+ * 
+ * @author Siddharth Sancheti, Section 33
+ * @version October 17, 2023
+ */
 public class SpaceFullException extends Exception {
     public SpaceFullException(String message) {
         super(message);

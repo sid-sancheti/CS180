a49d1613b9a55f7a06b0e1bd1673850809b713a8
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-10-24 16:58:52 -0400

    Challenge starting file

---
+++ Week10/Challenge/MapNavigator.java
@@ -0,0 +1,31 @@
+package Week10.Challenge;
+
+public class MapNavigator extends Thread {
+    public MapNavigator(int i, String file) {
+
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            MapNavigator[] mapNavigators = {
+                new MapNavigator(1 , "PlayerOneMoves.txt"),
+            new MapNavigator(2 , "PlayerTwoMoves.txt"), 
+            new MapNavigator(3 , "PlayerThreeMoves.txt"), 
+            new MapNavigator(4 , "PlayerFourMoves.txt")}; 
+    
+            for (int i = 0; i < mapNavigators.length; i++) {
+                mapNavigators[i].start();
+            }
+            for (int i = 0; i < mapNavigators.length; i++) {
+                mapNavigators[i].join();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+    }
+}

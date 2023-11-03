9fff4d0cba11329ac028ccb5b03e5e9ee325eaa6
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-11-03 01:39:55 -0400

    Refactor search functionality to allow for multiple searches and fix readLine issue

--- Week11/Challenge/SearchClient.java
+++ Week11/Challenge/SearchClient.java
@@ -2,10 +2,14 @@ package Week11.Challenge;
 import javax.swing.*;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 /**SearchClient.java
  * 
  * This class is the client side of the search engine. 
  * It prompts the user for a search query and sends it to the server.
+ * 
+ * HOST NAME: localhost
+ * PORT NUMBER: 1234
  * 
  * @author Siddharth Sancheti, Section 33
  * @version November 1, 2023
@@ -49,10 +53,44 @@ public class SearchClient {
             return;
         }
 
+        // TODO: Place in a do-while loop to allow the user to search multiple times.
         // Asking the user for their search query
-        String searchQuery = JOptionPane.showInputDialog(null, "Enter your search query: ", TITLE, JOptionPane.QUESTION_MESSAGE);
+        String searchQuery;
+        do {    // Verify whether the user inputed a query.
+            searchQuery = JOptionPane.showInputDialog(null, "Enter your search query: ", TITLE, JOptionPane.QUESTION_MESSAGE);
 
+            if (searchQuery == null || searchQuery.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please enter a search query!", TITLE, JOptionPane.ERROR_MESSAGE);
+            }
+        } while (searchQuery == null || searchQuery.isEmpty());
+
+        // Send the user's query to the server and receive the response
+        try {
+            // Send search query
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            out.println(searchQuery);
+
+            // Return query results
+            ArrayList<String> results = new ArrayList<String>();
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String response = in.readLine();
+
+            // TODO: There is something going on with the readLine function. I'm not sure what it is.
+
+            // Continue reading each line sent by the server until there are no more lines to read
+            while (response != null) {
+                results.add(response);
+                response = in.readLine();
+            }
+
+            
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "IOException thrown", TITLE, JOptionPane.ERROR_MESSAGE);
+            return;
+        }
         
+        // Close the server connection with a goodbye message
+        JOptionPane.showMessageDialog(null, "Goodbye User! Until next time...", TITLE, JOptionPane.INFORMATION_MESSAGE);
         try {
             client.close();
         } catch (IOException e) {
--- Week11/Challenge/SearchServer.java
+++ Week11/Challenge/SearchServer.java
@@ -9,6 +9,9 @@ import javax.swing.JOptionPane;
  * 
  * This class is the server side of the search engine.
  * 
+ * HOST NAME: localhost
+ * PORT NUMBER: 1234
+ * 
  * @author Siddharth Sancheti, Section 33
  * @version November 1, 2023
  */
@@ -17,16 +20,17 @@ public class SearchServer {
     public static void main(String[] args) {
 
         try (ServerSocket serverSocket = new ServerSocket(1234)) {
-
-            // Load the database from the "searchDatabase.txt" file
-            ArrayList<String> database = loadDatabase();
             
             while (true) {
                 Socket clientSocket = serverSocket.accept();
 
                 // Handle client's requests in a new thread
-                Thread clientThread = new Thread(() -> handleClient(clientSocket, database));
+                Thread clientThread = new Thread(() -> handleClient(clientSocket));
                 clientThread.start();
+
+
+
+
             }
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "IOException thrown", "Search Engine", JOptionPane.ERROR_MESSAGE);
@@ -35,7 +39,7 @@ public class SearchServer {
 
     private static ArrayList<String> loadDatabase() {
         ArrayList<String> database = new ArrayList<>();
-        // TODO: Change Week11/Challege/searchDatabase.txt to the correct path
+        // TODO: Change path before submission
         try (BufferedReader br = new BufferedReader(new FileReader("Week11/Challenge/searchDatabase.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -47,41 +51,49 @@ public class SearchServer {
         return database;
     }
 
-    private static void handleClient(Socket clientSocket, List<String> database) {
-        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    private static ArrayList<String> searchDatabase(String searchQuery) {
+        // The arrayList that contains all matching titles to the search query
+        ArrayList<String> matchingTitles = new ArrayList<>();
+        // Contains all the pages
+        ArrayList<String> database = loadDatabase();
 
-            String searchQuery = in.readLine();
-            List<String> matchingTitles = new ArrayList<>();
-            for (String entry : database) {
-                String[] parts = entry.split(";");
-                // The .contains() method determines whether a given phrase or word appears in a string
-                if (parts.length >= 2 && (parts[1].contains(searchQuery) || parts[2].contains(searchQuery))) {
-                    matchingTitles.add(parts[1]);
-                }
+        // Make the search query all lowercase
+        searchQuery = searchQuery.toLowerCase();
+
+        // Create a new ArrayList that is all lowercase
+        ArrayList<String> lowerCaseDB = new ArrayList<String>();
+        for (String entry : database) {
+            lowerCaseDB.add(entry.toLowerCase());
+        }
+
+        for (int i = 0; i < database.size(); ++i) {
+            String entry = lowerCaseDB.get(i);
+            String[] parts = entry.split(";");
+
+            // The .contains() method determines whether a given phrase or word appears in a string
+            if (parts[1].contains(searchQuery) || parts[2].contains(searchQuery)) {
+                matchingTitles.add(database.get(i));
+            }
+        }
+
+        return matchingTitles;
+    }
+
+    private static void handleClient(Socket clientSocket) {
+        try (PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String message = reader.readLine();
+
+            // Search find the titles that match the search query
+            ArrayList<String> matchingTitles = searchDatabase(message);
+
+            // Send the titles to the client
+            for (String title : matchingTitles) {
+                writer.println(title);
             }
 
-            if (matchingTitles.isEmpty()) {
-                out.println("Error");
-            } else {
-                for (String title : matchingTitles) {
-                    out.println(title);
-                }
-            }
-
-            String selectedTitle = in.readLine();
-            System.out.println("Client selected title: " + selectedTitle);
-
-            for (String entry : database) {
-                String[] parts = entry.split(";");
-                if (parts.length >= 2 && parts[1].equals(selectedTitle)) {
-                    out.println(parts[2]); // Send the description
-                    break;
-                }
-            }
-
-            in.close();
-            clientSocket.close();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
